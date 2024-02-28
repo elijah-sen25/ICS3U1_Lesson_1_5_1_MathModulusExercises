412f7e58d82cf7b343dc70c8478601433a741be2
elijah-sen25 <elijah.sen25@ycdsbk12.ca>
2024-02-27 20:24:38 -0500

    complete

--- Circumference.java
+++ Circumference.java
@@ -2,12 +2,25 @@ class Circumference extends ConsoleProgram {
 
   /**
   * Description
-  * @author:
+  * @author: Elijah Sen
   */
   
   public void run() {
-    
-    // start coding here
+    // Declare Variables
+    double dblRadius;
+    double dblCircumference;
+
+   // Gather Prompt
+    dblRadius = readDouble("Enter a radius: ");
+
+    // Calculate Circumference using prompt
+    dblCircumference = 2 * Math.PI * dblRadius;
+
+    // Display Answer
+
+    System.out.println("The Circumference is: " + dblCircumference);
+
+
     
   }
 }
