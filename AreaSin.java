f82ed51a386380ccd37267eee50ff8331518f60f
elijah-sen25 <elijah.sen25@ycdsbk12.ca>
2024-02-27 20:47:59 -0500

    complete

--- AreaSin.java
+++ AreaSin.java
@@ -5,8 +5,28 @@ class AreaSin extends ConsoleProgram {
   */
   
   public void run() {
+    // Declare Variables
+      double dblSideA;
+      double dblSideB;
+      double dblAngleC;
+      double dblSideC;
+
+    // Gather User Input
+
+      dblSideA = readDouble("Enter a value for A: ");
+      dblSideB = readDouble("Enter a value for B: ");
+      dblAngleC = readDouble("Enter a value for the given angle: ");
+
+
+    // Calculate Cosine Law
+
+      dblSideC = Math.sqrt(Math.pow(dblSideA, 2) + Math.pow(dblSideB, 2) - 2 * dblSideA * dblSideB * Math.cos(dblAngleC));
+
+    // Display Answer
+
+      System.out.println("The Value of Side C is " + dblSideC);
+      
     
-    // start coding here
-    
+  
   }
 }
